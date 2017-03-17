dcbb2f824f909cdb854e86589f07bf8fd2102ef4
Christiaan Quyn <christianq010@gmail.com>
2017-03-17 13:01:11 +0530

    refactor: README updated

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
 * scss - sass
  * watcher - converts when change detected
  * auto-prefixer - determine browser support from gulpfile
-* Live edits - browsersync detects and makes live changes by observing css
+* Live edits - browsersync detects and makes live changes by observing css [<https://browsersync.io/>]
+ * To run BrowserSync on a static HTML site - `browser-sync start --server --files "css/*.css"`
+ * To run BrowserSync on local host - `browser-sync start --proxy "myproject.dev" --files "css/*.css"`
--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,17 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
+// var autoprefixer = require('gulp-autoprefixer');
 var watch = require('gulp-watch');
-// var browserSync = require('browser-sync').create();
-//
-// browserSync.init({
-// 	server: "./"
-// });
-// browserSync.stream();
+
 
 gulp.task('default', function() {
-	gulp.watch('sass/**/*.scss',['styles']);
+	console.log('Hello World!');
 });
 
 gulp.task('styles', function() {
-	gulp.src('sass/**/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions']
-		}))
+return watch('sass/**/*.scss', { ignoreInitial: false })
 		.pipe(gulp.dest('./css'));
+		// .pipe(autoprefixer({
+		// 	browsers: ['last 2 versions']
+		// }))
 });
